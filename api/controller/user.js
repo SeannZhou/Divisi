22249af95aefe3cc6ece8e20a977690d45bc7f4b
Sean Zhou <seanzhou0@gmail.com>
2020-09-10 20:46:49 -0400

    - created user controller - added getAllUser endpoint

---
+++ api/controller/user.js
@@ -0,0 +1,76 @@
+module.exports.registerUser = function (req, res) {
+    const { errors, isValid } = validateRegisterInput(req.body);    // Form validation
+    if (!isValid) return res.status(400).json(errors)
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+            console.log(newUser)
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+            return res.status(httpStatus.CREATED);
+        }
+    });
+}
+
+module.exports.updateNameByEmail = function (req, res) {
+    let newName = req.params.name;
+    if (newName) {
+        User.updateOne({email: req.params.email}, {name: newName}).then(promise => {
+            if (promise.n == 1) {
+                return res.json({usobjecter: promise});
+            } else {
+                return res.status(httpStatus.NOT_FOUND).json({ error: `User with email ${req.params.email} does not exist`});
+            }
+        })
+    }
+}
+
+module.exports.getUser = function (req, res) {
+    User.findOne({email: req.params.email}).then(user => {
+        // console.log(user);
+        if (user){
+            return res.json({user: user});
+        } else {
+            // console.log(httpStatus.NOT_FOUND);
+            return res.status(httpStatus.NOT_FOUND).json({ error: `User with email ${req.params.email} does not exist`});
+        }
+    })
+}
+
+module.exports.getAllUser = function (req, res) {
+    User.find().then(user => {
+        // console.log(user);
+        if (user){
+            return res.json({user: user});
+        } else {
+            // console.log(httpStatus.NOT_FOUND);
+            return res.status(httpStatus.NOT_FOUND).json({ error: `User with email ${req.params.email} does not exist`});
+        }
+    })
+}
+
+module.exports.deleteUser = function (req, res) {
+    User.findOneAndDelete({ email: req.params.email }).then(user => {
+        if (user) {
+            return res.json({ user: user });
+        }else {
+            return res.status(400).json({ email: `user with email ${req.param.email} does not exist`});
+        }
+    })
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -16,70 +16,17 @@ const validateRegisterInput = require("../utils/register");
 //load models
 const User = require("../models/User");
 
+const controller = require("../controller/user");
+const { registerUser, updateNameByEmail, getUser, getAllUser, deleteUser } = controller;
 
-router.post("/register", (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);    // Form validation
-    if (!isValid) return res.status(400).json(errors)
+router.post("/register", registerUser);
 
-    User.findOne({ email: req.body.email }).then(user => {
-        if (user) {
-            return res.status(400).json({ email: "Email already exists" });
-        } else {
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            });
-            console.log(newUser)
-            // Hash password before saving in database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser
-                        .save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err));
-                });
-            });
-            return res.status(httpStatus.CREATED);
-        }
-    });
-});
+router.put("/:email/:name", updateNameByEmail)
 
-router.put("/:email/:name", (req, res) => {
-    let newName = req.params.name;
-    if (newName) {
-        User.updateOne({email: req.params.email}, {name: newName}).then(promise => {
-            if (promise.n == 1) {
-                return res.json({usobjecter: promise});
-            } else {
-                return res.status(httpStatus.NOT_FOUND).json({ error: `User with email ${req.params.email} does not exist`});
-            }
-        })
-    }
-})
+router.get('/:email', getUser)
 
-router.get('/:email', (req, res) => {
-    User.findOne({email: req.params.email}).then(user => {
-        // console.log(user);
-        if (user){
-            return res.json({user: user});
-        } else {
-            // console.log(httpStatus.NOT_FOUND);
-            return res.status(httpStatus.NOT_FOUND).json({ error: `User with email ${req.params.email} does not exist`});
-        }
-    })
-})
+router.get('', getAllUser)
 
-router.delete("/:email", (req, res) => {
-    User.findOneAndDelete({ email: req.params.email }).then(user => {
-        if (user) {
-            return res.json({ user: user });
-        }else {
-            return res.status(400).json({ email: `user with email ${req.param.email} does not exist`});
-        }
-    })
-})
+router.delete("/:email", deleteUser)
 
 module.exports = router;

ef816ad5ed22ea7fdf28472bac1c606e9f76ece0
internet <internet@szs-MacBook-Pro.local>
2020-11-04 00:22:14 -0500

    Adding other User fields

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
-const UserSchema = new Schema({
+const UserSchema = new Schema({     // Still need to add mixtapes, groups, friends
     name: {
         type: String,
         required: true
@@ -14,6 +14,18 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    profile_picture: {              // gender, country, profile_picture false for now
+        type: String,
+        required: false
+    },
+    gender: {
+        type: String,
+        required: false
+    },
+    country: {
+        type: String,
+        required: false
     }
 });
 

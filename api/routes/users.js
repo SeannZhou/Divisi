70e26e88347921d378de04fa18c51a019897d60c
internet <internet@szs-MacBook-Pro.local>
2020-11-04 00:09:03 -0500

    Setup /secure route to test secure routes for logged in users

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,14 +6,19 @@
  
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
  
  
 // load controllers
 const controller = require("../controllers/users");
 const { registerUser, loginUser, updateNameByEmail, getUser, deleteUser } = controller;
- 
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/secure", passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    console.log("user is logged in!");
+    res.status(200).json({ success: true, msg: "You are authorized"});
+});
 router.put("/:email/:name", updateNameByEmail);
 router.get('/user/:email', getUser);
 router.delete("/:email", deleteUser);

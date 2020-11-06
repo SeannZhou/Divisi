7865bd9e413f896112b79f09e063e5d203370abd
SeannZhou <seanzhou0@gmail.com>
2020-11-06 20:57:42 +0000

    Update mixtapes.js

--- api/routes/mixtapes.js
+++ api/routes/mixtapes.js
@@ -11,8 +11,8 @@ const controller = require("../controllers/mixtapes");
 const { createMixtape, getMixtape, addTrack, removeTrack } = controller;
 
 router.post("/mixtape", createMixtape);
-router.get('/mixtape/:id', passport.authenticate('jwt', {session: false}), getMixtape);
-router.patch('/mixtape/:id/addtrack', passport.authenticate('jwt', {session: false}), addTrack);
-router.patch('/mixtape/:id/removetrack', passport.authenticate('jwt', {session: false}), removeTrack);
+router.get('/mixtape/:id', getMixtape);
+router.patch('/mixtape/:id/addtrack', addTrack);
+router.patch('/mixtape/:id/removetrack', removeTrack);
 
 module.exports = router;

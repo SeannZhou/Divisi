8f7729b09c040ffedd8097c614dcde88fa7a8853
Archeane <jenny.xu@datadoghq.com>
2020-11-19 13:56:01 -0500

    search function completed

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -3,14 +3,17 @@ const httpStatus = require('http-status');
 // load models
 const Track = require("../models/Track");
 const Artist = require("../models/Artist");
+const User = require("../models/User");
+const Group = require("../models/Group");
+const Mixtape = require("../models/Mixtape");
 
 module.exports.getTrack = function (req, res) {
     console.log(req.params.id);
     Track.findById(req.params.id).then(track => {
-        if (track){
-            return res.status(httpStatus.OK).json({track: track});
+        if (track) {
+            return res.status(httpStatus.OK).json({ track: track });
         } else {
-            return res.status(httpStatus.NOT_FOUND).json({ error: `there are no tracks found with id ${req.params.id}`});
+            return res.status(httpStatus.NOT_FOUND).json({ error: `there are no tracks found with id ${req.params.id}` });
         }
     })
 }
@@ -18,37 +21,29 @@ module.exports.getTrack = function (req, res) {
 module.exports.getArtist = function (req, res) {
     console.log(req.params.id);
     Artist.findById(req.params.id).then(artist => {
-        if (artist){
-            return res.status(httpStatus.OK).json({artist: artist});
+        if (artist) {
+            return res.status(httpStatus.OK).json({ artist: artist });
         } else {
-            return res.status(httpStatus.NOT_FOUND).json({ error: `there are no artist found with id ${req.params.id}`});
+            return res.status(httpStatus.NOT_FOUND).json({ error: `there are no artist found with id ${req.params.id}` });
         }
     })
 }
 
 
-module.exports.search = (req, res) => {
+module.exports.search = async (req, res) => {
     let searchName = req.query.name;
-    Track.find({ title: { $regex: searchName, $options:'i' }}).then(results => {
-        console.log(results)
-        return res.status(httpStatus.OK).json({results: results});
-    });
+    var tracks = await Track.find({ title: { $regex: searchName, $options: 'i' } });
+    var artists = await Artist.find({ name: { $regex: searchName, $options: 'i' } });
+    var users = await User.find({ username: { $regex: searchName, $options: 'i' } });
+    var groups = await Group.find({ name: { $regex: searchName, $options: 'i' } });
+    var mixtapes = await Mixtape.find({ name: { $regex: searchName, $options: 'i' } });
 
-    // let collectionName_to_queryName = {
-    //     "users" : ["username"],
-    //     "artists": ["name"],
-    //     "groups": ["name"],
-    //     "mixtapes": ["name"],
-    //     "track": ["artist_name", "title"]
-    // };
+    let results = []
+    tracks.splice(0, 3).forEach(track => results.push({_id : track._id, title: track.title, picture: track.cover_picture, url: `/track/${track._id}`}));
+    artists.splice(0, 3).forEach(artist => results.push({_id : artist._id, title: artist.name, picture: artist.artist_cover, url: `/artist/${artist._id}`}));
+    users.splice(0, 3).forEach(user => results.push({_id : user._id, title: user.name, picture: user.profile_picture, url: `/user/${user._id}`}));
+    groups.splice(0, 3).forEach(group => results.push({_id : group._id, title: group.name, url: `/group/${group._id}`}));
+    mixtapes.splice(0, 3).forEach(mixtape => results.push({_id : mixtape._id, title: mixtape.name, picture: mixtape.mixtape_cover, url: `/mixtape/${mixtape._id}`}));
 
-    // let collectionName_to_queryResults = {};
-    // for (const [key, value] of Object.entries(collectionName_to_queryName)) {
-    //     console.log(key, value);
-    //     await db.
-    // }
-
-    // let userQuery = {
-    //     "username": { $regex: `/${searchName}/i` }
-    // }
+    return res.status(httpStatus.OK).json({ results: results});
 }
